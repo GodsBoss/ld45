74ea4b87038c964dad3da3c0e1ac260a29978220
Timo Reitz <godsboss@gmx.de>
2019-10-05 01:45:05 +0200

    Add image smoothing configuration capabilities to Context2D.

--- pkg/dom/canvas.go
+++ pkg/dom/canvas.go
@@ -37,3 +37,18 @@ type Context2D struct {
 	obj    *js.Object
 	canvas *Canvas
 }
+
+// EnableImageSmoothing enables image smoothing. This is also the default.
+func (ctx *Context2D) EnableImageSmoothing() *Context2D {
+	return ctx.xableImageSmoothing(true)
+}
+
+// DisableImageSmoothing disbles image smoothing, good for pixel art.
+func (ctx *Context2D) DisableImageSmoothing() *Context2D {
+	return ctx.xableImageSmoothing(false)
+}
+
+func (ctx *Context2D) xableImageSmoothing(value bool) *Context2D {
+	ctx.obj.Set("imageSmoothingEnabled", value)
+	return ctx
+}
