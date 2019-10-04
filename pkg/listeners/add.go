f4d0045628ef877d45b64226e8a03d17b7a35ae4
Timo Reitz <godsboss@gmx.de>
2019-10-05 00:37:58 +0200

    Add package for adding event listeners.

---
+++ pkg/listeners/add.go
@@ -0,0 +1,30 @@
+package listeners
+
+import (
+	"github.com/gopherjs/gopherjs/js"
+)
+
+// Add adds an event listener to the given object.
+func Add(
+	obj *js.Object,
+	eventType string,
+	f func(
+		this *js.Object,
+		arguments []*js.Object,
+	) interface{},
+) *js.Object {
+	g := func(this *js.Object, arguments []*js.Object) interface{} {
+		// Non-blocking, as recommended by https://github.com/gopherjs/gopherjs#goroutines
+		go f(this, arguments)
+		return nil
+	}
+	return obj.Call("addEventListener", eventType, js.MakeFunc(g), false)
+}
+
+// Simple wraps a function which does not need this or arguments when used as an event handler. It returns a function compatible with Add().
+func Simple(f func()) func(*js.Object, []*js.Object) interface{} {
+	return func(*js.Object, []*js.Object) interface{} {
+		f()
+		return nil
+	}
+}
