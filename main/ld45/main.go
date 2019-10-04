289648fa04290d52c1118636ed97818faf49e1a4
Timo Reitz <godsboss@gmx.de>
2019-10-05 00:41:01 +0200

    Add NOP initialization.

--- main/ld45/main.go
+++ main/ld45/main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"github.com/GodsBoss/ld45/pkg/listeners"
+
+	"github.com/gopherjs/gopherjs/js"
+)
+
+func main() {
+	listeners.Add(js.Global.Get("window"), "load", initialize)
+}
+
+func initialize(this *js.Object, arguments []*js.Object) interface{} {
+	return nil
+}
