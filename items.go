73e968587dfce2b4e6461ece7abf7fc9a2e1e652
Timo Reitz <godsboss@gmx.de>
2019-10-06 00:39:36 +0200

    Add helper function for creating item positions.

--- items.go
+++ items.go
@@ -1,5 +1,10 @@
 package ld45
 
+import (
+	"math"
+	"math/rand"
+)
+
 type itemID string
 
 func (id itemID) New(x, y float64) *item {
@@ -43,4 +48,10 @@ func (i *item) ToObjects(cam camera) []Object {
 			GroundBound: true,
 		},
 	}
+}
+
+func randomPositionAround(x, y, minR, maxR float64) (float64, float64) {
+	angle := rand.Float64() * math.Pi * 2
+	d := rand.Float64()*(maxR-minR) + minR
+	return x + math.Sin(angle)*d, y + math.Cos(angle)*d
 }
