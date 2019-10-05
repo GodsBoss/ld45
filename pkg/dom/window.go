2d834e81ba8b57ca4451162234bf2182afda793a
Timo Reitz <godsboss@gmx.de>
2019-10-05 04:02:57 +0200

    Window allows for setting key events.

--- pkg/dom/window.go
+++ pkg/dom/window.go
@@ -1,6 +1,8 @@
 package dom
 
 import (
+	"github.com/GodsBoss/ld45/pkg/listeners"
+
 	"github.com/gopherjs/gopherjs/js"
 )
 
@@ -19,5 +21,20 @@ func (window *Window) SetTimeout(f func(), delayInMS int) *Window {
 		go f()
 	}
 	window.obj.Call("setTimeout", g, delayInMS)
+	return window
+}
+
+func (window *Window) OnKeyDown(f func(listeners.KeyEvent)) *Window {
+	listeners.Add(window.obj, "keydown", listeners.WithKeyEvent(f))
+	return window
+}
+
+func (window *Window) OnKeyUp(f func(listeners.KeyEvent)) *Window {
+	listeners.Add(window.obj, "keyup", listeners.WithKeyEvent(f))
+	return window
+}
+
+func (window *Window) OnKeyPress(f func(listeners.KeyEvent)) *Window {
+	listeners.Add(window.obj, "keypress", listeners.WithKeyEvent(f))
 	return window
 }
