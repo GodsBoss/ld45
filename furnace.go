dcf4c6f1e0d0e502c49712fa2de33db0cdd6094c
Timo Reitz <godsboss@gmx.de>
2019-10-06 01:41:32 +0200

    Furnace can start burning items.

--- furnace.go
+++ furnace.go
@@ -4,11 +4,41 @@ type furnace struct {
 	nopOnPlayerContact
 	positionPartial
 
-	lifetime int
+	burning     bool
+	burningItem itemID
+	lifetime    int
 }
 
 func (furn *furnace) Interactions() []interaction {
-	return make([]interaction, 0)
+	if furn.burning {
+		return make([]interaction, 0)
+	}
+	return []interaction{
+		newSimpleInteraction(
+			possibleAll(
+				minimalInventory(itemCoal, 1),
+				minimalInventory(itemIronOre, 1),
+			),
+			func(_ int, p *playing) {
+				furn.burning = true
+				furn.burningItem = itemIronOre
+				p.player.inventory[itemCoal]--
+				p.player.inventory[itemIronOre]--
+			},
+		),
+		newSimpleInteraction(
+			possibleAll(
+				minimalInventory(itemCoal, 1),
+				minimalInventory(itemGoldOre, 1),
+			),
+			func(_ int, p *playing) {
+				furn.burning = true
+				furn.burningItem = itemGoldOre
+				p.player.inventory[itemCoal]--
+				p.player.inventory[itemGoldOre]--
+			},
+		),
+	}
 }
 
 func (furn *furnace) Tick(ms int) {
@@ -17,11 +47,15 @@ func (furn *furnace) Tick(ms int) {
 
 func (furn *furnace) ToObjects(cam camera) []Object {
 	x, y := calculateScreenPosition(cam, furn.x, furn.y)
+	key := "furnace_off"
+	if furn.burning {
+		key = "furnace_burning"
+	}
 	return []Object{
 		{
 			X:           x,
 			Y:           y,
-			Key:         "furnace_off",
+			Key:         key,
 			GroundBound: true,
 			Lifetime:    furn.lifetime,
 		},
