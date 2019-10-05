c3adcb7d367774999c47e481950600f4e66e6d10
Timo Reitz <godsboss@gmx.de>
2019-10-05 07:52:11 +0200

    (More or less) complete main().

--- main/ld45/main.go
+++ main/ld45/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"github.com/GodsBoss/ld45"
 	"github.com/GodsBoss/ld45/pkg/console"
+	"github.com/GodsBoss/ld45/pkg/date"
 	"github.com/GodsBoss/ld45/pkg/dom"
 	"github.com/GodsBoss/ld45/pkg/listeners"
+	"github.com/GodsBoss/ld45/pkg/loop"
+	"github.com/GodsBoss/ld45/ui"
 
 	"github.com/gopherjs/gopherjs/js"
 )
@@ -30,5 +34,87 @@ func initialize(this *js.Object, arguments []*js.Object) interface{} {
 			"width":        "800px",
 		},
 	)
+	img := doc.CreateImage()
+	img.OnLoad(
+		func() {
+			tps := 50
+			game := ld45.NewGame()
+			timed := &loop.Timed{
+				CurrentTimeInMS: func() int {
+					return date.Now().Unix()
+				},
+				ScheduleStep: func(f func(), delay int) {
+					dom.GlobalWindow().SetTimeout(f, delay)
+				},
+			}
+			timed.Start(
+				func() {
+					game.Tick(1000 / tps)
+				},
+				1000/tps,
+			)
+
+			ctx := canvas.GetContext2D()
+			ctx.DisableImageSmoothing()
+			renderer := &ui.Renderer{
+				Zoom:          2,
+				Ctx:           ctx,
+				ImageSource:   dom.ImageElementSource(img),
+				SpriteMapping: map[string]ui.Sprite{},
+				BackgroundMapping: map[string]dom.FillStyle{
+					"title":     dom.Color("#447722"),
+					"playing":   dom.Color("#447722"),
+					"game_over": dom.Color("#447722"),
+				},
+			}
+			simple := &loop.Simple{
+				ScheduleStep: func(f func()) {
+					dom.GlobalWindow().RequestAnimationFrame(
+						func(_ float64) {
+							f()
+						},
+					)
+				},
+			}
+			simple.Start(
+				func() {
+					renderer.Draw(game)
+				},
+			)
+			keyEventMapping := map[string]ld45.KeyEventType{
+				"keyup":    ld45.KeyUp,
+				"keydown":  ld45.KeyDown,
+				"keypress": ld45.KeyPress,
+			}
+			passKeyToGame := func(event listeners.KeyEvent) {
+				if event.Repeat() {
+					return
+				}
+				eventType, ok := keyEventMapping[event.Type()]
+				if !ok {
+					return
+				}
+				game.InvokeKeyEvent(
+					ld45.KeyEvent{
+						Type:  eventType,
+						Alt:   event.AltKey(),
+						Ctrl:  event.CtrlKey(),
+						Shift: event.ShiftKey(),
+						Key:   event.Key(),
+					},
+				)
+			}
+			dom.GlobalWindow().
+				OnKeyDown(passKeyToGame).
+				OnKeyUp(passKeyToGame).
+				OnKeyPress(passKeyToGame)
+		},
+	)
+	img.OnError(
+		func(event listeners.Event) {
+			console.Log("error", event.Type())
+		},
+	)
+	img.Src("gfx.png")
 	return nil
 }
