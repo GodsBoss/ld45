65636e5e955918ec1e8774a9681841b422bcd399
Timo Reitz <godsboss@gmx.de>
2019-10-05 15:49:06 +0200

    Implement trees.

---
+++ tree.go
@@ -0,0 +1,35 @@
+package ld45
+
+import "fmt"
+
+type tree struct {
+	x float64
+	y float64
+
+	growth       intProperty
+	fluentGrowth float64
+}
+
+func (t *tree) Tick(ms int) {
+	if !t.growth.IsMaximum() {
+		t.fluentGrowth += float64(ms) / 1000.0
+		if t.fluentGrowth >= treeGrowCost {
+			t.growth.Inc(1)
+			t.fluentGrowth = 0
+		}
+	}
+}
+
+const treeGrowCost = 60.0
+
+func (t *tree) ToObjects(cam camera) []Object {
+	x, y := calculateScreenPosition(cam, t.x, t.y)
+	return []Object{
+		{
+			X:        x,
+			Y:        y,
+			Key:      fmt.Sprintf("tree_%d", t.growth.current),
+			Lifetime: 0,
+		},
+	}
+}
