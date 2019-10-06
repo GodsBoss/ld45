48f7bbd418c40aedcae3e7c117a99de7932f7a16
Timo Reitz <godsboss@gmx.de>
2019-10-06 19:28:35 +0200

    Enhance game over state.

--- gameOver.go
+++ gameOver.go
@@ -3,18 +3,56 @@ package ld45
 type gameOver struct {
 	choice *characterChoice
 	result *playResult
+
+	objects []Object
 }
 
 func (over *gameOver) ID() string {
 	return "game_over"
 }
 
-func (over *gameOver) Init() {}
+func (over *gameOver) Init() {
+	if over.result.IsVictory() {
+		over.initVictoryObjects()
+	} else {
+		over.initDefeatObjects()
+	}
+}
+
+func (over *gameOver) initVictoryObjects() {
+	over.objects = []Object{
+		{
+			X:   190,
+			Y:   130,
+			Key: "game_over_crown",
+		},
+		{
+			X:   157,
+			Y:   150,
+			Key: "game_over_victory_header",
+		},
+	}
+}
+
+func (over *gameOver) initDefeatObjects() {
+	over.objects = []Object{
+		{
+			X:   192,
+			Y:   130,
+			Key: "game_over_gravestone",
+		},
+		{
+			X:   140,
+			Y:   155,
+			Key: "game_over_defeat_header",
+		},
+	}
+}
 
 func (over *gameOver) Tick(ms int) {}
 
 func (over *gameOver) Objects() []Object {
-	return make([]Object, 0)
+	return over.objects
 }
 
 func (over *gameOver) InvokeKeyEvent(event KeyEvent) {}
