cb2edc07315555babbaf1f81ceec02187e75e782
Timo Reitz <godsboss@gmx.de>
2019-10-06 11:10:38 +0200

    Add recipes for workbench.

--- workbench.go
+++ workbench.go
@@ -11,7 +11,11 @@ func (wb *workbench) ID() interactibleID {
 }
 
 func (wb *workbench) Interactions() []interaction {
-	return make([]interaction, 0)
+	result := make([]interaction, len(recipes))
+	for i := range recipes {
+		result[i] = recipes[i].toInteraction()
+	}
+	return result
 }
 
 func (wb *workbench) ToObjects(cam camera) []Object {
@@ -23,5 +27,120 @@ func (wb *workbench) ToObjects(cam camera) []Object {
 			Key:         "workbench",
 			GroundBound: true,
 		},
+	}
+}
+
+type recipe struct {
+	key       interactionID
+	input     map[itemID]int
+	output    func(*playing)
+	condition func(*player) bool
+}
+
+func (r *recipe) toInteraction() interaction {
+	return newSimpleInteraction(
+		r.key,
+		false,
+		func(p *player) bool {
+			if r.condition != nil && !r.condition(p) {
+				return false
+			}
+			for inputItemID := range r.input {
+				if p.inventory[inputItemID] < r.input[inputItemID] {
+					return false
+				}
+			}
+			return true
+		},
+		func(_ int, p *playing) {
+			r.output(p)
+		},
+	)
+}
+
+var recipes = []recipe{
+	{
+		key:       "interaction_axe_wood",
+		input:     map[itemID]int{itemWood: 4},
+		output:    workbenchToolOutput(toolAxe, toolWood),
+		condition: maximumToolQuality(toolAxe, toolNone),
+	},
+	{
+		key:       "interaction_pickaxe_wood",
+		input:     map[itemID]int{itemWood: 4},
+		output:    workbenchToolOutput(toolPickaxe, toolWood),
+		condition: maximumToolQuality(toolPickaxe, toolNone),
+	},
+	{
+		key:       "interaction_sword_wood",
+		input:     map[itemID]int{itemWood: 4},
+		output:    workbenchToolOutput(toolSword, toolWood),
+		condition: maximumToolQuality(toolSword, toolNone),
+	},
+	{
+		key: "interaction_axe_stone",
+		input: map[itemID]int{
+			itemWood: 1,
+			itemRock: 3,
+		},
+		output:    workbenchToolOutput(toolAxe, toolStone),
+		condition: maximumToolQuality(toolAxe, toolWood),
+	},
+	{
+		key: "interaction_pickaxe_stone",
+		input: map[itemID]int{
+			itemWood: 1,
+			itemRock: 3,
+		},
+		output:    workbenchToolOutput(toolPickaxe, toolStone),
+		condition: maximumToolQuality(toolPickaxe, toolWood),
+	},
+	{
+		key: "interaction_sword_stone",
+		input: map[itemID]int{
+			itemWood: 1,
+			itemRock: 3,
+		},
+		output:    workbenchToolOutput(toolSword, toolStone),
+		condition: maximumToolQuality(toolSword, toolWood),
+	},
+	{
+		key: "interaction_axe_iron",
+		input: map[itemID]int{
+			itemWood:      1,
+			itemIronIngot: 3,
+		},
+		output:    workbenchToolOutput(toolAxe, toolIron),
+		condition: maximumToolQuality(toolAxe, toolStone),
+	},
+	{
+		key: "interaction_pickaxe_iron",
+		input: map[itemID]int{
+			itemWood:      1,
+			itemIronIngot: 3,
+		},
+		output:    workbenchToolOutput(toolPickaxe, toolIron),
+		condition: maximumToolQuality(toolPickaxe, toolStone),
+	},
+	{
+		key: "interaction_sword_iron",
+		input: map[itemID]int{
+			itemWood:      1,
+			itemIronIngot: 3,
+		},
+		output:    workbenchToolOutput(toolSword, toolIron),
+		condition: maximumToolQuality(toolSword, toolStone),
+	},
+}
+
+func workbenchToolOutput(id toolID, quality toolQuality) func(*playing) {
+	return func(p *playing) {
+		p.player.equipment[id] = quality
+	}
+}
+
+func maximumToolQuality(id toolID, quality toolQuality) func(*player) bool {
+	return func(p *player) bool {
+		return p.equipment[id] <= quality
 	}
 }
