311968d68fbc1152c387e333e2051094d0403323
Timo Reitz <godsboss@gmx.de>
2019-10-06 20:55:35 +0200

    Add sectors helper.

---
+++ sectors.go
@@ -0,0 +1,70 @@
+package ld45
+
+import (
+	"math"
+)
+
+// sectors is responsible for generating objects in neighbouring sectors when
+// the player enters a specific sector.
+type sectors struct {
+	alreadyGenerated map[sectorID]struct{}
+	sectorWidth      float64
+	sectorHeight     float64
+	generate         func(id sectorID, left, top, right, bottom float64)
+}
+
+func newSectors(sectorWidth, sectorHeight float64, generate func(id sectorID, left, top, right, bottom float64)) *sectors {
+	return &sectors{
+		sectorWidth:      sectorWidth,
+		sectorHeight:     sectorHeight,
+		generate:         generate,
+		alreadyGenerated: make(map[sectorID]struct{}),
+	}
+}
+
+func (s *sectors) playerMovesTo(x, y float64) {
+	currentSector := s.positionToSectorID(x, y)
+	if _, ok := s.alreadyGenerated[currentSector]; ok {
+		return
+	}
+	candidates := currentSector.sectorIncludingNeighbours()
+	for i := range candidates {
+		s.attemptGenerate(candidates[i])
+	}
+}
+
+func (s *sectors) positionToSectorID(x, y float64) sectorID {
+	return sectorID{
+		X: int(math.Floor(x/s.sectorWidth + 0.5)),
+		Y: int(math.Floor(y/s.sectorHeight + 0.5)),
+	}
+}
+
+func (s *sectors) attemptGenerate(id sectorID) {
+	if _, ok := s.alreadyGenerated[id]; ok {
+		return
+	}
+	cx, cy := float64(id.X)*s.sectorWidth, float64(id.Y)*s.sectorHeight
+	s.generate(id, cx-s.sectorWidth*0.5, cy-s.sectorHeight*0.5, cx+s.sectorWidth*0.5, cy+s.sectorHeight*0.5)
+	s.alreadyGenerated[id] = struct{}{}
+}
+
+type sectorID struct {
+	X int
+	Y int
+}
+
+func (id sectorID) sectorIncludingNeighbours() []sectorID {
+	ids := make([]sectorID, 9)
+	index := 0
+	for dx := -1; dx <= 1; dx++ {
+		for dy := -1; dy <= 1; dy++ {
+			ids[index] = sectorID{
+				X: id.X + dx,
+				Y: id.Y + dy,
+			}
+			index++
+		}
+	}
+	return ids
+}
