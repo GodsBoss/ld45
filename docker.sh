8074e5bbc041cde72a899ec7f2ce68e89fcb458a
Timo Reitz <godsboss@gmx.de>
2019-10-13 21:50:45 +0200

    Add build helper command.

--- docker.sh
+++ docker.sh
@@ -17,3 +17,15 @@ then
     godsboss/gopherjs \
     gopherjs serve -v github.com/GodsBoss/ld45/main/ld45
 fi
+
+if [ "$1" == "build" ]
+then
+  docker run \
+    --rm \
+    -it \
+    -u 1000:1000 \
+    --mount type=bind,src=${PWD},dst=/go/src/github.com/GodsBoss/ld45 \
+    --workdir /go/src/github.com/GodsBoss/ld45 \
+    godsboss/gopherjs \
+    gopherjs build --output ./main/ld45/ld45.js --minify --verbose github.com/GodsBoss/ld45/main/ld45
+fi
